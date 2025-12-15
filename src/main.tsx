e68ccf08b7438edb3e2857dac810266bd7dbc3b1
naimrifat <167227892+naimrifat@users.noreply.github.com>
2025-12-15 13:17:45 +0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-console.log('Supabase URL:', import.meta.env.VITE_SUPABASE_URL);
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+console.log('Supabase URL:', import.meta.env.VITE_SUPABASE_URL);
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
