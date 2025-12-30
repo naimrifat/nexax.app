98bfcb4509e93c614b76843d6e41701592b5fd13
naimrifat <167227892+naimrifat@users.noreply.github.com>
2025-12-30 15:40:24 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // src/components/ProtectedRoute.tsx
 import React from "react";
 import { Navigate, useLocation } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from ".../context/AuthContext";
 
 type ProtectedRouteProps = {
   children: React.ReactNode;
