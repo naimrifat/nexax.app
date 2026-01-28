453c03ca1e54dd8c29d19658ab3eca5b9d980396
naimrifat <167227892+naimrifat@users.noreply.github.com>
2026-01-28 15:54:49 +0600

    Update reconcilePrompt.ts

--- lib/prompts/reconcilePrompt.ts
+++ lib/prompts/reconcilePrompt.ts
@@ -33,6 +33,204 @@ Variant / Size (if applicable and clearly known)
 Condition (only if clearly known)
 Do NOT change this order.
 
+PREOWNED_TITLE_EXAMPLES:
+
+Evening Dresses:
+"Reformation Juliette Dress Women's Size 6 Black Midi Silk Blend Cocktail Party"
+
+Designer Pieces:
+"Gucci Marmont Matelasse Leather Belt Bag Black Gold Hardware Crossbody Authentic"
+
+Denim:
+"Levi's 501 Original Fit Women's Jeans Size 28x30 Light Wash Vintage Distressed"
+
+Activewear:
+"Lululemon Align High Rise Leggings Size 6 Black Nulu 25 Inch Full Length Yoga"
+
+Vintage Fashion:
+"Vintage 1980s Oversized Denim Jacket Women's M Acid Wash Boyfriend Style Retro"
+
+Handbags:
+"Coach Madison Pebbled Leather Tote Bag Brown Shoulder Purse Zip Top Authentic"
+
+**MEN'S CLOTHING**
+
+Shirts:
+"Polo Ralph Lauren Custom Fit Oxford Shirt Men's Large Blue White Striped Cotton"
+
+Outerwear:
+"Patagonia Better Sweater Quarter Zip Fleece Men's Medium Navy Blue Jacket Layer"
+
+Sneakers:
+"Nike Air Jordan 1 Retro High OG Men's Size 10.5 Black Toe White Red 2016 Release"
+
+Suits:
+"Hugo Boss Wool Suit 42R Charcoal Gray Two Button Single Breast Super 120s Jacket"
+
+Jeans:
+"Diesel Thavar Slim Skinny Jeans Men's 32x32 Dark Wash Stretch Denim Italian"
+
+**ELECTRONICS & TECH (22% of Pre-Owned)**
+
+Smartphones:
+"Apple iPhone 12 Pro 128GB Pacific Blue Unlocked A2341 5G AT&T T-Mobile Verizon"
+
+Laptops:
+"Dell Latitude 7490 14 Inch Laptop Intel i5 8th Gen 8GB RAM 256GB SSD Windows 11"
+
+Gaming Consoles:
+"Sony PlayStation 4 Pro 1TB Console CUH-7215B Black with Controller HDMI Cables"
+
+Tablets:
+"Apple iPad Air 4th Generation 64GB WiFi 10.9 Inch Space Gray 2020 Model A2316"
+
+Smartwatches:
+"Apple Watch Series 6 44mm Space Gray Aluminum GPS Sport Band Black Health Fitness"
+
+Cameras:
+"Canon EOS Rebel T7 DSLR Camera 24.1MP with 18-55mm Lens Kit Battery Charger Strap"
+
+Gaming Accessories:
+"Xbox Elite Wireless Controller Series 2 Black for Xbox One Series X S PC Paddles"
+
+Fiction Hardcover:
+"Harry Potter Complete Series Box Set JK Rowling Hardcover 1-7 Bloomsbury UK First"
+
+Textbooks:
+"Campbell Biology 12th Edition Hardcover Urry Cain 2020 College Textbook AP Study"
+
+Collectible Books:
+"To Kill a Mockingbird Harper Lee First Edition 1960 Hardcover with Dust Jacket"
+
+Paperback Series:
+"Lord of the Rings Trilogy JRR Tolkien Complete Set Paperback Movie Tie-In Edition"
+
+Athletic:
+"Nike Air Max 90 Women's Size 8 Triple White Leather Mesh Running Training Casual"
+
+Designer Heels:
+"Christian Louboutin So Kate 120mm Heels Size 38.5 Black Patent Leather Pumps Red"
+
+Boots:
+"Red Wing Heritage Iron Ranger Boots Men's Size 10D Amber Harness Leather 8111"
+
+Sandals:
+"Birkenstock Arizona Soft Footbed Sandals Women's 38 EU Taupe Suede Leather Buckle"
+
+Casual Sneakers:
+"Adidas Superstar Original Men's Size 11 White with Black Stripes Shell Toe Classic"
+
+Kitchen Appliances:
+"KitchenAid Artisan Series 5 Quart Tilt Head Stand Mixer KSM150 Empire Red Baking"
+
+Furniture:
+"Mid Century Modern Walnut Coffee Table Danish Style Tapered Legs 48x20 Living Room"
+
+Garden Tools:
+"Stihl MS 170 Chainsaw 16 Inch Bar Gas Powered 30.1cc Easy Start Orange Protective"
+
+Home Decor:
+"Vintage Brass Candlestick Holders Set of 4 Tall Patina 12 Inch Taper Candle Decor"
+
+Power Tools:
+"DeWalt DCD771C2 20V Max Cordless Drill Driver Kit Lithium Ion Battery Charger Case"
+
+**COLLECTIBLES & TRADING CARDS**
+
+Pokemon Cards:
+"Pokemon Charizard VMAX 020/189 Darkness Ablaze Ultra Rare Holo Foil Card Mint"
+
+Sports Cards:
+"Mike Trout 2011 Topps Update Rookie Card US175 RC Angels Hall of Fame PSA Ready"
+
+Vintage Toys:
+"Star Wars Vintage Kenner Millennium Falcon 1979 Complete with Box Instructions"
+
+Funko Pop:
+"Funko Pop Marvel Avengers Endgame Iron Man 467 Amazon Exclusive GITD Glow Chase"
+
+Comic Books:
+"Amazing Spider-Man 300 1st Venom May 1988 Todd McFarlane Marvel Comics VF Grade"
+
+Coins:
+"1921 Morgan Silver Dollar Philadelphia Mint 90% Silver US Coin Circulated Fine"
+
+**VIDEO GAMES & CONSOLES**
+
+Video Games:
+"Legend of Zelda Breath of Wild Nintendo Switch Game Cartridge Case Complete 2017"
+
+Retro Gaming:
+"Nintendo GameBoy Color Atomic Purple Handheld Console GBC Tested Working 1998"
+
+PC Games:
+"World of Warcraft Collector's Edition PC Blizzard 2004 Sealed Art Book Soundtrack"
+
+**JEWELRY & WATCHES**
+
+Fine Jewelry:
+"14K Yellow Gold Diamond Solitaire Engagement Ring 0.50ct Round Cut Size 6 SI1"
+
+Fashion Jewelry:
+"Tiffany & Co Return to Tiffany Heart Tag Toggle Necklace Sterling Silver 16 Inch"
+
+Men's Watches:
+"Seiko 5 Sports Automatic Watch SRPD55 Blue Dial Steel Bracelet 100m Water Resist"
+
+Vintage Watches:
+"Omega Seamaster Automatic Date Men's Watch Stainless Steel 1970s Vintage Serviced"
+
+**SPORTING GOODS**
+
+Golf:
+"Titleist Pro V1 Golf Balls Prior Generation Dozen White 2022 Tour Performance Used"
+
+Fitness Equipment:
+"Bowflex SelectTech 552 Adjustable Dumbbells Pair 5-52.5 lbs Weight Dial System"
+
+Bicycles:
+"Trek Marlin 7 Mountain Bike 29er Medium Frame Blue Shimano Deore 2x9 Speed Trail"
+
+Camping:
+"Coleman Sundome 6 Person Tent 10x10 Feet WeatherTec System Rainfly Camping Family"
+
+**MUSICAL INSTRUMENTS**
+
+Guitars:
+"Fender Stratocaster American Standard Electric Guitar Sunburst Maple Neck Rosewood"
+
+Keyboards:
+"Yamaha P-125 88-Key Weighted Action Digital Piano Black with Sustain Pedal Stand"
+
+Drums:
+"Pearl Export 5 Piece Drum Set Black Cherry Complete with Hardware Cymbals Throne"
+
+**BABY & KIDS**
+
+Strollers:
+"UPPAbaby Vista V2 Stroller Lucy Rose Gold Bassinet Toddler Seat Rain Cover 2020"
+
+Car Seats:
+"Britax Boulevard ClickTight Convertible Car Seat SafeWash Fabric Gray 5-65 lbs"
+
+Toys:
+"LEGO Star Wars Millennium Falcon 75192 Ultimate Collector Series 7541 Pieces New"
+
+Clothing:
+"Gap Kids Boys Winter Puffer Jacket Size 8 Navy Blue Hooded Zip Front Warm Coat"
+
+**AUTOMOTIVE PARTS**
+
+OEM Parts:
+"Genuine Toyota 04152-YZZA1 Oil Filter 4-Pack Factory OEM Camry Corolla RAV4 Tacoma"
+
+Aftermarket:
+"K&N 33-2304 High Performance Air Filter for Toyota Honda Nissan Reusable Washable"
+
+Tools:
+"Snap-On 3/8 Drive Air Impact Wrench MG325 80 ft-lbs Composite Pneumatic Tool Blue"
+
+
 HARD CONSTRAINTS
 Maximum length: 80 characters
 Target length: 60–75 characters
