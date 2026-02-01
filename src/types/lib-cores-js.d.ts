c85115180967d2856f21bc73d43a8fc2ad07e52f
naimrifat <167227892+naimrifat@users.noreply.github.com>
2026-02-01 12:55:20 +0600

    Create lib-cores-js.d.ts

---
+++ src/types/lib-cores-js.d.ts
@@ -0,0 +1,24 @@
+declare module '../lib/cores/analyzeListingCore.js' {
+  export const analyzeListingCore: any
+}
+
+declare module '../lib/cores/reconcileSpecificsCore.js' {
+  export const reconcileSpecificsCore: any
+}
+
+declare module '../lib/cores/publishListingCore.js' {
+  export const publishListingCore: any
+}
+
+declare module '../lib/cores/transcribeCore.js' {
+  export const transcribeCore: any
+}
+
+declare module '../lib/cores/ebayCategoriesCore.js' {
+  export const ebayCategoriesCore: any
+}
+
+declare module '../lib/telemetry.js' {
+  const Telemetry: any
+  export default Telemetry
+}
