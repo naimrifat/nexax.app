c7cbb66a6231deb774fc34bae0eb5f3bb1640c70
naimrifat <167227892+naimrifat@users.noreply.github.com>
2026-02-01 12:34:06 +0600

    Create ebayCategoriesConfig.ts

---
+++ lib/cores/ebayCategoriesConfig.ts
@@ -0,0 +1,23 @@
+const ebayCategoriesConfig: Record<string, any[]> = {
+  '0': [
+    { name: 'Brand', required: false, multi: false, type: 'FreeText', values: [] },
+    { name: 'Model', required: false, multi: false, type: 'FreeText', values: [] },
+    { name: 'Color', required: false, multi: false, type: 'FreeText', values: [] },
+    { name: 'Size', required: false, multi: false, type: 'FreeText', values: [] },
+    { name: 'Pattern', required: false, multi: false, type: 'FreeText', values: [] },
+  ],
+  '1000': [
+    { name: 'Brand', required: false, multi: false, type: 'FreeText', values: [] },
+    { name: 'Size', required: false, multi: false, type: 'FreeText', values: [] },
+    { name: 'Color', required: false, multi: false, type: 'FreeText', values: [] },
+    { name: 'Pattern', required: false, multi: false, type: 'FreeText', values: [] },
+  ],
+  '2000': [
+    { name: 'Brand', required: false, multi: false, type: 'FreeText', values: [] },
+    { name: 'Model', required: false, multi: false, type: 'FreeText', values: [] },
+    { name: 'Color', required: false, multi: false, type: 'FreeText', values: [] },
+    { name: 'Voltage', required: false, multi: false, type: 'FreeText', values: [] },
+  ]
+}
+
+export default ebayCategoriesConfig
